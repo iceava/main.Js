cf0968bfd28cca0a81acf8fb34c6ac8b900bcd66
Ivan Dovbenco <mr.phenomen.pro@gmail.com>
2020-08-22 16:20:31 +0300

    first-comment

---
+++ main.js
@@ -0,0 +1,93 @@
+
+//var user = 'John', 
+//age = 25, 
+//message = 'Hello';
+//console.log(user, age, message)
+//alert ( message)
+
+/*var myNum = 25,
+    myString = 'Some string',
+    myBool = true,
+    nyNul = null,
+    myUndef = undefined;
+
+    console.log( myNum, myString, typeof myBool, nyNul, myUndef)
+   */
+
+   /*var n = 100
+   n = n / 20;
+   n = n + 20;
+   n = n * 20;
+
+   console.log(n)
+*/
+/*
+var n =100
+n /=20;
+n +=20;
+n *=21;
+
+console.log(n)
+
+console.log( 10/2 *10 >= 10)
+console.log(10=='10')
+console.log(10==='10')
+
+console.log('John 25'.length)
+
+console.log('Some new \
+string')
+
+var string = 'Lorem ipsum dolor sit amet,\
+consectetur adipisicing';
+console.log(string.replace('sit amet', 'not sit'));
+*/
+/*
+var myNum = 10
+var myStr = 'строка'
+var myBool = true
+var myArr = [1, 2, 3, 4, 5]
+var myObj = {first: 'First Name', last: 'Last Name'}
+var decimal2 = myNum.toFixed(2) 
+var i = i++
+        ++i
+        --i
+        i--
+
+        var myTest =20
+myTest *= myNum
+myTest /= 38
+myTest +=18
+myTest -=28
+myTest %= myNum
+
+var myPi = (Math.PI)
+console.log(myPi)
+
+var myRound = ( Math.round(89.279) )
+console.log(myRound)
+
+var myRandom = (Math.random()*10)
+console.log(myRandom)
+
+var myPow = (Math.pow(3,5))
+console.log(myPow)
+
+        */
+/*
+var strObj = {str:"Мама мыла раму, рама мыла маму"};
+strObj.length = strObj.str.length
+
+var isRamaPos = strObj.str.indexOf('рама')
+console.log(isRamaPos)
+*/
+/*
+var strReplace = strObj.str.replace("Мама мыла раму, рама мыла маму", "Мама моет раму, Рама держит маму")
+console.log(strReplace)
+
+strObj.str.toUpperCase()
+console.log(strObj.str)
+
+strObj.str.toLowerCase()
+console.log(strObj.str)
+*/
